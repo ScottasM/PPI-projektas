aaaafaf66bae42fc549a6c0cd78f1b3f4def8dca
ZygimantasVidmantas <zygimantas.vidmantas@mif.stud.vu.lt>
2023-09-14 19:30:48 +0300

    Created Note class
    Created basic implementation of Note class with rudimentary methods

---
+++ PPI-projektas/ClientApp/src/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Note
+{
+	//all pragma warnings are to be removed when User class is implemented
+	#pragma warning disable CS0246
+	const User author = null;
+	#pragma warning restore CS0246
+
+	public List<string> tags { get; private set; }
+	string location;
+
+	#pragma warning disable CS0246
+	#pragma warning disable CS0176
+	public Note(User author)
+	{
+		this.author = author;
+		tags = new List<string>();
+		location = "";	//Temporary, will be updated when database becomes available
+		//File will be created here
+	}
+	#pragma warning restore CS0246
+	#pragma warning restore CS0176
+	
+
+	public void addTag(string tag) => tags.Add(tag);
+
+	public void removeTag(string tag) => tags.Remove(tag);
+
+	public void updateNote(string text) => File.WriteAllText(location, text);
+}
