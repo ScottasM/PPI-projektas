5d2955a23a715584c5e9cec196f658f29fa1f164
Jokūbas Šilgalys <119012257+jokubassilgalys@users.noreply.github.com>
2023-09-14 21:03:45 +0300

    Add files via upload
    Created User class

---
+++ PPI-projektas/objects/User.cs
@@ -0,0 +1,27 @@
+﻿public class User
+{
+    private string username;
+    private readonly string password;
+    private readonly string email;
+
+    public List<Note> createdNotes;
+    public List<Note> favoriteNotes;
+
+    public User(string name, string password, string email)
+    {
+        this.username = name;
+        this.password = password;
+        this.email = email;
+        this.createdNotes = new List<Note>();
+        this.favoriteNotes = new List<Note>();
+    }
+
+    public string getUsername() => username;
+    public void setUsername(string name) => username = name;
+
+    public void addCreatedNote(Note note) => createdNotes.Add(note);
+    public void removeCreatedNote(Note note) => createdNotes.Remove(note);
+
+    public void addFavoriteNote(Note note) => favoriteNotes.Add(note);
+    public void removeFavoriteNote(Note note) => favoriteNotes.Remove(note);
+}
