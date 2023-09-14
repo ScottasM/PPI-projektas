9caa0310edcbbba69e6c487e0edb8ba02f72a887
Titas Stankevičius <stank.titas@gmail.com>
2023-09-14 20:53:13 +0300

    Create Group.cs

---
+++ PPI-projektas/objects/Group.cs
@@ -0,0 +1,44 @@
+namespace PPI_projektas;
+
+public class Group
+{
+    public readonly Guid Id;
+    public string Name { get; set; }
+    /*
+    public User owner { get; set; }
+    public List<User> members { get; }
+    */
+    public List<Note> notes { get; }
+
+    public Group(string name/*, User owner*/)
+    {
+        Id = new Guid();
+        Name = name;
+        notes = new List<Note>();
+        // this.owner = owner;
+        // members = new List<User>();
+    }
+    /*
+    public Group(string name, User owner, List<User> members) : this(name, owner)
+    {
+        this.members = members;
+    }
+    */
+
+    /*
+    public void CreateNote(User author)
+    {
+        Note newNote = new Note(author);
+        notes.Add(newNote);
+    }
+    /*
+
+    public void AddNote(Note note) => notes.Add(note);
+
+    public void RemoveNote(Note note) => notes.Remove(note);
+    /*
+    public void AddUser(Note note) => members.Add(note);
+
+    public void RemoveUser(Note note) => members.Remove(note);
+    */
+}
