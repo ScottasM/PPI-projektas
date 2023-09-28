9c347587843df4f58ed98fee714355a7d9aa5d98
Titas Stankevičius <stank.titas@gmail.com>
2023-09-28 12:42:48 +0300

    added context menu

--- PPI-projektas/ClientApp/src/components/Group.js
+++ PPI-projektas/ClientApp/src/components/Group.js
@@ -6,12 +6,42 @@ export class Group extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            isContextMenuVisible: false,
+        }
     }
 
+    showContextMenu = (event) => {
+        event.preventDefault();
+        this.setState({ isContextMenuVisible: true });
+        
+        document.addEventListener('click', this.hideContextMenu);
+    }
+    hideContextMenu = () => {
+        this.setState({ isContextMenuVisible: false });
+        document.removeEventListener('click', this.hideContextMenu);
+    }
+    
     render() {
+        const { isContextMenuVisible } = this.state;
+
         return (
-            <div className="group bg-white rounded-circle">
-                <p>{this.props.groupInitials}</p>
+            <div className="group-container">
+                <button className="group bg-white rounded-circle" onContextMenu={this.showContextMenu}>
+                    {this.props.groupInitials}
+                </button>
+                {isContextMenuVisible && (
+                    <div className="context-menu">
+                        <ul>
+                            <li>
+                                <button className="context-button">Edit</button>
+                            </li>
+                            <li>
+                                <button className="context-button">Delete</button>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         );
     }
