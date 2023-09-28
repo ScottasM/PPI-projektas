08fdcca74a8605ce9d26fbb69fa304c1a2d83c9f
Jokūbas Šilgalys <119012257+jokubassilgalys@users.noreply.github.com>
2023-09-28 20:07:25 +0300

    Update MainContainer.js

--- PPI-projektas/ClientApp/src/components/mainDiv/MainContainer.js
+++ PPI-projektas/ClientApp/src/components/mainDiv/MainContainer.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import {GroupCreateMenu} from "./GroupCreateMenu";
-import {CreatingButtons} from "./CreatingButtons";
+import { GroupCreateMenu } from "./GroupCreateMenu";
+import { UserLoginMenu } from "./UserLoginMenu";
+import { UserSignInMenu } from "./UserSignInMenu";
+import { CreatingButtons } from "./CreatingButtons";
+import { CreatingLoginButtons } from "./CreatingLoginButtons";
+import { CreatingSignInButtons } from "./CreatingSignInButtons";
 export class MainContainer extends Component {
     static displayName = MainContainer.name;
 
 
     state = {
         displayGroupCreateMenu: false,
+        displayLoginMenu: false,
+        displaySignInMenu: false,
     };
     
     toggleGroupCreateMenu = () => {
+        if (!(this.displayGroupCreateMenu)) {
+            this.setState({ displayLoginMenu: false, displaySignInMenu: false })
+        }
+
         this.setState((prevState) => ({
             displayGroupCreateMenu: !prevState.displayGroupCreateMenu,
         }));
+    }
+
+    toggleLoginMenu = () => {
+        if (!(this.displayLoginMenu)) {
+            this.setState({ displayGroupCreateMenu: false, displaySignInMenu: false })
+        }
+
+        this.setState((prevState) => ({
+            displayLoginMenu: !prevState.displayLoginMenu,
+        }));
+    }
+
+    toggleSignInMenu = () => {
+        if (!(this.displaySignInMenu)) {
+            this.setState({ displayGroupCreateMenu: false, displayLoginMenu: false })
+        }
+
+        this.setState((prevState) => ({
+            displaySignInMenu: !prevState.displaySignInMenu,
+        }));
+ 
     }
     
     render() {
         return (
             <div className="bg-white">
-                <CreatingButtons toggleMenu={this.toggleGroupCreateMenu}/>
+                <CreatingButtons toggleMenu={this.toggleGroupCreateMenu} />
                 {this.state.displayGroupCreateMenu && <GroupCreateMenu />}
+
+                <CreatingSignInButtons toggleMenu={this.toggleSignInMenu} />
+                {this.state.displaySignInMenu && <UserSignInMenu />}
+
+                <CreatingLoginButtons toggleMenu={this.toggleLoginMenu} />
+                {this.state.displayLoginMenu && <UserLoginMenu />}
             </div>
         );
     }
