dfc2d09f2143df720b46f2a0b81e560947c37e20
Scottas <68515176+ScottasM@users.noreply.github.com>
2023-09-28 23:30:21 +0300

    Password hashing + registering

--- PPI-projektas/Services/AuthenticationService.cs
+++ PPI-projektas/Services/AuthenticationService.cs
@@ -1,7 +1,36 @@
-﻿namespace PPI_projektas.Services
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using PPI_projektas.objects;
+using PPI_projektas.Utils;
+
+namespace PPI_projektas.Services
 {
     public class AuthenticationService
     {
+        public User TryRegister(string name, string password, string email)
+        {
+            string hashedPassword = hash(password);
+
+            User newUser = new User(name,password,email);
+            DataHandler.Create(newUser);
+
+            return newUser;
+        }
+
+
+        private string hash(string str)
+        {
+
+            byte[] inputBytes = Encoding.UTF8.GetBytes(str);
+
+            using (SHA256 sha256 = SHA256.Create()) {
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                string hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                return hashString;
+            }
+        }
 
     }
 }
