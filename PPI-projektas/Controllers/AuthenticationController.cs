e20cb46878687ff427d48210cbafeccc2d62e617
Scottas <68515176+ScottasM@users.noreply.github.com>
2023-09-28 23:57:54 +0300

    Create AuthenticationController.cs

---
+++ PPI-projektas/Controllers/AuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PPI_projektas.objects;
+using PPI_projektas.Utils;
+using PPI_projektas.Services;
+
+namespace PPI_projektas.Controllers
+{
+    
+    public class AuthenticationController
+    {
+        AuthenticationService? service;
+
+        public void Register()
+        {
+            service = LazySingleton<AuthenticationService>.Instance;
+        }
+    }
+}
