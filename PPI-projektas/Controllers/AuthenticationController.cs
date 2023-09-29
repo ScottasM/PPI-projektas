ed7aa9c2afe5d7484318407a8715fc67ca813285
Scottas <68515176+ScottasM@users.noreply.github.com>
2023-09-29 08:58:54 +0300

    Update AuthenticationController.cs

--- PPI-projektas/Controllers/AuthenticationController.cs
+++ PPI-projektas/Controllers/AuthenticationController.cs
@@ -3,16 +3,55 @@ using PPI_projektas.objects;
 using PPI_projektas.Utils;
 using PPI_projektas.Services;
 
+
 namespace PPI_projektas.Controllers
 {
-    
-    public class AuthenticationController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController : ControllerBase
     {
         AuthenticationService? service;
 
-        public void Register()
+
+        [HttpPost("tryregister")]
+        public IActionResult Register([FromBody]AuthData authData)
         {
             service = LazySingleton<AuthenticationService>.Instance;
+            
+            AuthReturn authReturn = service.TryRegister(authData.Username, authData.Password);
+            if(authReturn == null) {
+                return BadRequest("Register failed. Please try again later");
+            }
+
+            if (!authReturn.success) {
+                return BadRequest(authReturn.errorMessage);
+            }
+
+            return CreatedAtAction("Register", new { id = authReturn.user.Id, username = authReturn.user.GetUsername() }, authReturn.user);
         }
+
+
+        [HttpPost("trylogin")]
+        public IActionResult Login([FromBody]AuthData authData)
+        {
+            service = LazySingleton<AuthenticationService>.Instance;
+
+            AuthReturn authReturn = service.TryLogin(authData.Username, authData.Password);
+            if (authReturn == null) {
+                return BadRequest("Login failed. Please try again later");
+            }
+
+            if (!authReturn.success) {
+                return BadRequest(authReturn.errorMessage);
+            }
+
+            return CreatedAtAction("Login", new { id = authReturn.user.Id, username = authReturn.user.GetUsername() }, authReturn.user);
+        }
+    }
+
+    public class AuthData
+    {
+        public string Username;
+        public string Password;
     }
 }
