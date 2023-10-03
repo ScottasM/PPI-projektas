3d862d1df18e175923bd3e898c9c2d52fe2ceb70
Titas Stankevičius <stank.titas@gmail.com>
2023-10-03 18:59:40 +0300

    Sending group ids together with names

--- PPI-projektas/Controllers/GroupController.cs
+++ PPI-projektas/Controllers/GroupController.cs
@@ -11,25 +11,33 @@ namespace PPI_projektas.Controllers
     [Route("api/[controller]")]
     public class GroupController : ControllerBase
     {
+        
         [HttpGet]
         public IActionResult Get(Guid ownerId)
         {
+                
+                
             var groups = DataHandler.Instance.AllGroups;
             var groupNames = groups
-                //.Where(group => group.OwnerGuid == ownerId) Will be uncommented then user is associated on the frontend
+                //.Where(group => group.OwnerGuid == ownerId) Will be uncommented when user is associated on the frontend
                 .Select(group => group.Name)
                 .ToList();
-            return Ok(groupNames);
+            var groupIds = groups
+                //.Where(group => group.OwnerGuid == ownerId) Will be uncommented when user is associated on the frontend
+                .Select(group => group.Id)
+                .ToList();
+            
+            var groupData = new GroupData(groupNames, groupIds);
+            
+            return Ok(groupData);
         }
 
         [HttpPost("creategroup")]
-        public IActionResult CreateGroup([FromBody] GroupData groupData)
+        public IActionResult CreateGroup([FromBody] GroupCreateData groupData)
         {
             if (groupData == null) return BadRequest("Invalid Data");
 
             if (string.IsNullOrEmpty(groupData.OwnerId.ToString())) return BadRequest("OwnerId is empty or null");
-
-            Console.WriteLine("HttpPost " + groupData.GroupName);
 
             var owner = new User("test", " ", " "); // temporary user for all groups
             DataHandler.Create(owner);
@@ -41,7 +49,32 @@ namespace PPI_projektas.Controllers
         }
     }
 
+    public class GroupDataItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public class GroupData
+    {
+        public List<GroupDataItem> Groups { get; set; }
+
+        public GroupData(List<string> groupNames, List<Guid> groupIds)
+        {
+            Groups = new List<GroupDataItem>();
+
+            for (var i = 0; i < Math.Min(groupNames.Count, groupIds.Count); i++)
+            {
+                Groups.Add(new GroupDataItem
+                {
+                    Id = groupIds[i],
+                    Name = groupNames[i]
+                });
+            }
+        }
+    }
+    
+    public record GroupCreateData
     {
         public string GroupName { get; set; }
         public Guid OwnerId { get; set; }
