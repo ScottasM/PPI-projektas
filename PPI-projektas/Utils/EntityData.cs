ce887b622f6c3c18db07ca1a472aad3e9006ef6e
Scottas <68515176+ScottasM@users.noreply.github.com>
2023-10-26 17:27:24 +0300

    entity setup

---
+++ PPI-projektas/Utils/EntityData.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PPI_projektas.objects;
+using System;
+
+namespace PPI_projektas.Utils
+{
+    public class EntityData : DbContext
+    {
+        public  EntityData(DbContextOptions<EntityData> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
