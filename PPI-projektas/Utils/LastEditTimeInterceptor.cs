45b040ed5ebe9ca11c55a80b2ca81e5b64b231c1
Scottas <68515176+ScottasM@users.noreply.github.com>
2023-12-11 19:43:52 +0200

    Interceptor

---
+++ PPI-projektas/Utils/LastEditTimeInterceptor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using PPI_projektas.objects;
+
+namespace PPI_projektas.Utils
+{
+    public class LastEditTimeInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            var context = eventData.Context;
+            var entries = context.ChangeTracker.Entries<Note>();
+
+            foreach (var entry in entries) {
+                if (entry.State == EntityState.Modified) {
+                    entry.Entity.LastEditTime = DateTime.Now;
+                }
+            }
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
